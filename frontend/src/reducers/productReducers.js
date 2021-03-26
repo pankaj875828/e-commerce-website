ca15cd6be37950eaad7a7f97b93a3bfde7506e37
pankaj8758 <pankajgodhani99@gmail.com>
2021-03-26 22:59:25 +0530

    add redux to pruct screen

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,4 +1,4 @@
-const { PRODUCT_LIST_REQUEST ,PRODUCT_LIST_SUCCESS,PRODUCT_LIST_FAIL } = require("../constants/productConstants");
+const { PRODUCT_LIST_REQUEST ,PRODUCT_LIST_SUCCESS,PRODUCT_LIST_FAIL,PRODUCT_DETAILS_FAIL,PRODUCT_DETAILS_SUCCESS,PRODUCT_DETAILS_REQUEST } = require("../constants/productConstants");
 
 
 export const productListReducer = (state = {loading:true,products:[]},action) =>{
@@ -8,6 +8,19 @@ export const productListReducer = (state = {loading:true,products:[]},action) =>
         case PRODUCT_LIST_SUCCESS :
             return {loading:false,products:action.payload}
         case PRODUCT_LIST_FAIL :
+            return {loading:false,error:action.payload}
+        default:
+            return state;
+    }
+}
+
+export const productDeatailsReducer = (state = {products:{},loading:true},action) =>{
+    switch(action.type){
+        case PRODUCT_DETAILS_REQUEST : 
+            return {loading:true}
+        case PRODUCT_DETAILS_SUCCESS :
+            return {loading:false,products:action.payload}
+        case PRODUCT_DETAILS_FAIL :
             return {loading:false,error:action.payload}
         default:
             return state;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,11 @@
 import {applyMiddleware, combineReducers, compose, createStore} from 'redux';
 import thunk from 'redux-thunk'
-import { productListReducer } from "./reducers/productReducers";
+import { productListReducer, productDeatailsReducer } from "./reducers/productReducers";
 const initialState ={};
 
 const reducer =combineReducers({
     productList : productListReducer,
+    productDetails : productDeatailsReducer,
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
